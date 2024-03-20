ce5c84bc89f4e7a61721b93c4f99311520a2f095
doheeing <doheeehan@naver.com>
2024-03-21 00:11:26 +0900

    승패까지 가리기

--- src/App.js
+++ src/App.js
@@ -1,37 +1,73 @@
 import { useState } from "react";
 import "./App.css";
 import Box from "./component/Box";
+
 const choice = {
-  rock : {
-    name : "rock",
-    img : "https://i0.wp.com/DaveGipson.net/wp-content/uploads/2021/11/img_0271.jpg?fit=980%2C653&ssl=1&w=640"
+  rock: {
+    name: "rock",
+    img: "https://akash.dev/rps/img/rock-right.png",
   },
-  scissor:{
-    name : "scissor",
-    img : "https://cdn.imweb.me/thumbnail/20200514/7fc8b1411fa8d.png"
+  scissor: {
+    name: "scissor",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQL5xt_damKrVj0cWAjda417qoicTVUT0sdWA&usqp=CAU",
   },
-  paper:{
-    name : "paper",
-    img : "https://m.media-amazon.com/images/I/61OorFhm6SL._AC_UF894,1000_QL80_.jpg"
-  }
-}
+  paper: {
+    name: "paper",
+    img: "https://www.pngitem.com/pimgs/m/592-5920636_rock-paper-scissors-clipart-rock-paper-scissors-png.png",
+  },
+};
 function App() {
-  const [userSelect, setUserSelect] = useState(null)
+  const [userSelect, setUserSelect] = useState(null);
+  const [ComputorSelect, setComputorSelect] = useState(null);
+  const [result, setResult] = useState("");
+  const [comResult, setComResult] = useState("");
+  const [resultBox, setResultBox] = useState("");
+  const play = (userChoice) => {
+    setUserSelect(choice[userChoice]);
+    let computorChoice = randomChoice();
+    setComputorSelect(computorChoice);
+    setResult(judgement(choice[userChoice], computorChoice));
+    setComResult(reverseJudgement(choice[userChoice], computorChoice));
+  };
+  const judgement = (user, computor) => {
+    if (user.name === computor.name) {
+      return "tie";
+    } else if (user.name === "rock")
+      return computor.name === "scissor" ? "win" : "lose";
+    else if (user.name === "paper")
+      return computor.name === "rock" ? "win" : "lose";
+    else if (user.name === "scissor")
+      return computor.name === "paper" ? "win" : "lose";
+  };
+  const reverseJudgement = (user, computor) => {
+    if (user.name === computor.name) {
+      return "tie";
+    } else if (user.name === "rock")
+      return computor.name === "scissor" ? "lose" : "win";
+    else if (user.name === "paper")
+      return computor.name === "rock" ? "lose" : "win";
+    else if (user.name === "scissor")
+      return computor.name === "paper" ? "lose" : "win";
+  };
+  const randomChoice = () => {
+    let itemArray = Object.keys(choice);
+    let randomItem = Math.floor(Math.random() * itemArray.length);
+    let final = itemArray[randomItem];
+    return choice[final];
+  };
 
-
-  const play = (userChoice) =>{
-    setUserSelect(choice[userChoice])
-  }
   return (
-    <div>
-      <div className="main">
-        <Box title="You" item={userSelect}/>
-        {/* <Box title="Computor" item={userSelect}/> */}
-      </div>
-      <div className="button">
-      <button onClick={()=>play("scissor")}>가위</button>
-      <button onClick={()=>play("rock")}>바위</button>
-      <button onClick={()=>play("paper")}>보</button>
+    <div className="containor">
+      <div className="mainbox">
+        <div className="main">
+          <Box title="You" item={userSelect} result={result} />
+          <Box title="Computor" item={ComputorSelect} result={comResult} />
+        </div>
+        <div className="button">
+          <button className="buttoneach" onClick={() => play("scissor")}>가위</button>
+          <button className="buttoneach"onClick={() => play("rock")}>바위</button>
+          <button className="buttoneach" onClick={() => play("paper")}>보</button>
+        </div>
       </div>
     </div>
   );
